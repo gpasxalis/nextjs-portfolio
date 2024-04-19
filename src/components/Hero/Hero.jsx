a215fcec356fce636ba51b5ed40e6cd69a4cafe2
gpasxalis <gpasxalis94@gmail.com>
2024-04-19 11:53:28 +0300

    first commit

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,101 @@
+"use client";
+
+import {React, useCallback} from "react";
+import SectionSubtitle from "../utils/SectionSubtitle";
+import { Container, Row, Col } from "reactstrap";
+import Image from "next/image";
+import Link from "next/link";
+import heroImg from "../../../public/images/hero.jpg";
+import classes from "./hero.module.css"
+
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+import particleConfig from "../utils/particles.json";
+
+const Hero = () => {
+  const particlesInit = useCallback(async (engine) => {
+    await loadFull(engine);
+  }, []);
+
+
+  return (
+    <section className={`${classes.hero}`}>
+      <Particles
+          id="tsparticles"
+          init={particlesInit}
+          options={particleConfig}
+          position="absolute"
+          zIndex={0}
+        />
+      <Container>
+        <Row>
+          {/* ========== hero content ============= */}
+          <Col lg="6" md="6">
+            <div className={`${classes.hero__content}`}>
+              <SectionSubtitle subtitle="Hello" />
+              <h2 className="mt-3 mb-3 white_heading">I&apos;m Paschalis Grammenos</h2>
+              <h5 className="mb-4">Frontend Developer</h5>
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Impedit rem sint ipsa cumque. Atque rem vel iusto impedit omnis
+                quos!
+              </p>
+              <div className="mt-5">
+                <button className="primary__btn">
+                  <Link href="#">Hire me</Link>
+                </button>
+
+                <button className="secondary__btn">
+                  <Link href="#">Download CV</Link>
+                </button>
+              </div>
+            </div>
+          </Col>
+
+          {/* ========== hero img ============= */}
+          <Col lg="6" md="6">
+            <div className={`${classes.hero__img} text-end`}>
+              <Image alt="hero-image" src={heroImg} width="400" height="400" />
+
+              <div className={`${classes.hero__skills}`}>
+                <h6>Skills</h6>
+                <span>
+                  <i className="ri-bar-chart-line"></i>
+                </span>
+                <span>
+                  <i className="ri-bar-chart-line"></i>
+                </span>
+                <span>
+                  <i className="ri-bar-chart-line"></i>
+                </span>
+                <span>
+                  <i className="ri-bar-chart-line"></i>
+                </span>
+                <span>
+                  <i className="ri-bar-chart-line"></i>
+                </span>
+                <span>
+                  <i className="ri-bar-chart-line"></i>
+                </span>
+              </div>
+
+              <div
+                className={`${classes.hero__experience} d-flex align-items-center gap-3`}
+              >
+                <span>
+                  <i className="ri-lightbulb-flash-line"></i>
+                </span>
+                <div className="bg-transparent">
+                  <h6 className="white_heading">Experience</h6>
+                  <h5 className="mb-0 white_heading">4 Years</h5>
+                </div>
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
+
+export default Hero;
