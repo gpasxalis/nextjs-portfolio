5c86404532d671211a16c2448eef0ddaeb824ab3
gpasxalis <gpasxalis94@gmail.com>
2024-04-22 17:08:19 +0300

    New Page Title

--- src/components/utils/data.js
+++ src/components/utils/data.js
@@ -43,42 +43,6 @@ const portfolio = [
     keyword: ["Mobile", "App", "UI-UX"],
     liveUrl: "#",
   },
-
-  {
-    id: "06",
-    title: "Travel  App",
-    img: "/images/portfolio-06.jpg",
-    category: "Mobile App",
-    keyword: ["Mobile", "App", "UI-UX"],
-    liveUrl: "#",
-  },
-
-  {
-    id: "07",
-    title: "Portfolio Website",
-    img: "/images/portfolio-07.png",
-    category: "Web Design",
-    keyword: ["Website", "Web design", "UI-UX"],
-    liveUrl: "#",
-  },
-
-  {
-    id: "08",
-    title: "Restaurant Website",
-    img: "/images/portfolio-08.png",
-    category: "Web Design",
-    keyword: ["Website", "Web design", "UI-UX"],
-    liveUrl: "#",
-  },
-
-  {
-    id: "09",
-    title: "Agency Website",
-    img: "/images/portfolio-09.png",
-    category: "Web Design",
-    keyword: ["Website", "Web design", "UI-UX"],
-    liveUrl: "#",
-  },
 ];
 
 export default portfolio;
